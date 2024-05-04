c3b53a9d49b142d18436b03c2bdc1cb2ab78bfd0
Yazan Alqadery <yazanalqadery@gmail.com>
2024-05-04 23:21:09 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const getAdviceBtn = document.querySelector(".get-advice");
+const adviceNumber = document.querySelector(".advice-number");
+const adviceText = document.querySelector(".advice-text");
+
+// Fetch advice from the API
+const getAdvice = async () => {
+  try {
+    adviceText.textContent = "Loading...";
+    const advice = await fetch("https://api.adviceslip.com/advice");
+    // Check if the response is ok
+    if (!advice.ok) {
+      throw new Error("Cannot fetch the data");
+    }
+    const adviceResponse = await advice.json();
+    adviceText.textContent = `"${adviceResponse.slip.advice}"`;
+    adviceNumber.textContent = `Advice #${adviceResponse.slip.id}`;
+  } catch (error) {
+    console.error(error);
+    adviceText.textContent = "Oops! Something went wrong!";
+    adviceNumber.textContent = "";
+  }
+};
+
+getAdviceBtn.addEventListener("click", getAdvice);
